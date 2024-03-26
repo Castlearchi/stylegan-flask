a9d21740fe5e44ffbfa23131af9157556c09c3b1
Castlearchi <k.useforwork@gmail.com>
2024-03-26 15:24:42 +0900

    ユーザー入力とGETによる遷移の実装

--- app/app.py
+++ app/app.py
@@ -1,20 +1,15 @@
-#Flaskとrender_template（HTMLを表示させるための関数）をインポート
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 
-#Flaskオブジェクトの生成
 app = Flask(__name__)
 
-
-#「/」へアクセスがあった場合に、"Hello World"の文字列を返す
 @app.route("/")
-def hello():
-    return "Hello Worldです"
-
-
-#「/index」へアクセスがあった場合に、「index.html」を返す
-@app.route("/index")
-def index():
+def indexHandler():
     return render_template("index.html")
+
+@app.route("/result", methods=["GET"])
+def resultHandler():
+    inputNum = request.args.get("userinput", type=int)
+    return render_template("result.html", seed=inputNum)
 
 
 #おまじない
